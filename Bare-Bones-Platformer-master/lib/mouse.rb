54bdd5896a76550f9c2cab11c6fa8b9aafd8c462
Alex <xanderstrike@gmail.com>
2013-02-12 15:45:44 -0800

    clean up

--- Bare-Bones-Platformer-master/lib/mouse.rb
+++
@@ -1,9 +0,0 @@
-class Mouse
-  def initialize(window)
-    @cursor = Gosu::Image.new(window, "#{IMAGES_DIR}/cursor.png", false)
-  end
-
-  def draw(mouse_x, mouse_y)
-    @cursor.draw(mouse_x, mouse_y, ZOrder::HUD)
-  end
-end
--- Bare-Bones-Platformer-master/platformer.rb
+++
@@ -1,4 +0,0 @@
-require './lib/engine.rb'
-
-window = GameWindow.new
-window.show()
