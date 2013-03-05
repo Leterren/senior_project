cb55fe3cdac1d119cb4b8d3804c6108b25477ac9
Benn <benn@Thomas-Mint.(none)>
2013-03-05 15:19:28 -0800

    minor changes

--- lib/game.rb
+++ lib/game.rb
@@ -23,7 +23,7 @@ class Game < Gosu::Window
 
   GRAVITY = 0.4
 
-  INITIAL_LEVEL = 'test'
+  INITIAL_LEVEL = 'dark'
 
   attr_accessor :window, :space, :objects, :state, :camera, :main_font, :editor, :victorystate
 
